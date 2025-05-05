c32dfc0e0f325365a605dc7b650b5ee81dc8bb21
langeshkumar <langeshkumar8@gmail.com>
2025-05-05 22:01:20 +0530

    server page

--- server.js
+++ server.js
@@ -3,8 +3,29 @@ import express from 'express';
 const app = express();
 const PORT = 6363;
 
+// sample router 
 app.get('/', (req, res) => {
     res.json({ msg: "This is get method..!" });
+});
+
+// crud process
+// R - for read process 
+app.get('', (req, res) => {
+    // 
+});
+
+// C - for create process 
+app.post('', (req, res) => {
+    // 
+});
+
+// U - for update process 
+app.put('', (req, res) => {
+    // 
+});
+// D - for delete process 
+app.delete('', (req, res) => {
+    // 
 });
 
 app.listen(PORT, () => {
